8819963ce0504404b77f41045a3ed3cd7e5d8c04
Sergey Kozlov <s.kozlov@blackmana.com>
2025-03-01 05:56:47 +0300

    rows and columns

--- BattleField/Utils/Global.swift
+++ BattleField/Utils/Global.swift
@@ -6,7 +6,7 @@
 //
 
 enum Global {
-    static var FIELD_COLUMNS: Int = 3
-    static var FIELD_ROWS: Int = 2
+    static var FIELD_COLUMNS: Int = 8
+    static var FIELD_ROWS: Int = 5
     static var STEP_DISTANCE : Int = 1
 }
